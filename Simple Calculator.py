9a5f562be916a7b2289be9b3beba6a2cddb95f3f
Adrita Dutta <adritad0820@gmail.com>
2025-06-17 13:11:07 +0530

    Update Simple Calculator.py

--- Simple Calculator.py
+++ Simple Calculator.py
@@ -1,22 +1,26 @@
-a=int(input("Enter 1st integer"))
-b=int(input("ENter 2nd integer"))
+try:
+    a = int(input("Enter 1st integer: "))
+    b = int(input("Enter 2nd integer: "))
+except ValueError:
+    print("Bro. That's not an integer.")
+    exit()
 print("Enter + for addition, - for subtraction, * for multiplication, / for division, and ^ for exponents")
 ch=input("Enter choice of operator")
 if ch == '+':
-    print(a+b)
+    print("Result",a+b)
 elif ch == '-':
-    print(a-b)
+    print("Result",a-b)
 elif ch == '*':
-    print(a*b)
+    print("Result",a*b)
 elif ch == '/':
     if b == 0:
         print("Division by zero, it is error")
     else:
-        print(a/b)
+        print("Result",a/b)
 elif ch == '^':
-    if a == 0:
-        print("Invalid")
+    if a == 0 and b == 0:
+        print("0 raised to 0 is... mathematically cursed.")
     else:
-        print(a**b)
+        print("Result",a**b)
 else:
     print("I am not paid enough to do that")
